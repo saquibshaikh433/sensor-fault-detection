ab94350a0fd52343d6177433c42deff8966c3eb7
Najmus Saquib <saquibshaikh433@gmail.com>
2023-04-17 17:25:32 +0530

    adding setuop

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup # find packages find the root packages
+
+setup(
+    name = "sensor_package",  # name of the package 
+    version = "0.0.1",          # version of ur package 
+    author = "najmu saquib",
+    author_email = "saquibshaikh433@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
+
